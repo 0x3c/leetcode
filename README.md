6254cf85da17f97560306cc22de7ee534ab14b4a
SamuelChia <funwithes6@gmail.com>
2019-07-30 22:41:42 +0800

    Add two sum

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # leetcode-ts
+
 typescript solutions for Leetcode problems
+
+### Topics
+
+[Array^1^]()
+
+### Algorithms
+
+| Title                         | Topics | Difficulty |
+| ----------------------------- | ------ | ---------- |
+| [1. Two Sum]('/algorithms/1') | Array  | Easy       |
---
+++ algorithms/1/index.ts
@@ -0,0 +1,20 @@
+interface TwoSumFn {
+  (nums: number[], target: number): number[];
+}
+
+export const twoSumFn: TwoSumFn = (nums, target) => {
+  let lp = 0,
+    rp = 0;
+  while (lp < nums.length - 1) {
+    while (++rp < nums.length) {
+      if (nums[lp] + nums[rp] === target) {
+        return [lp, rp];
+      }
+    }
+    rp = ++lp;
+  }
+  throw new Error("no result");
+};
+
+const result = twoSumFn([2, 7, 11, 15], 18);
+console.log(result);
