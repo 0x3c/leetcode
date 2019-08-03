442c7fb6ec6d9fe9a16e33c67dc694bcd7714898
SamuelChia <funwithes6@gmail.com>
2019-08-03 23:41:57 +0800

    Add 350 README

---
+++ algorithms/350/intersectionOfTwoArrays2.ts
@@ -0,0 +1,9 @@
+type IntersectionOfTwoArraysFn = (nums1: number[], numss: number[]) => number[];
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+const intersectionOfTwoArraysFn: IntersectionOfTwoArraysFn = (nums1, nums2) => {
+  return [];
+};
