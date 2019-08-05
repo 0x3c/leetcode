6eb64cf603075f189f5725abb6ffed5e42d89d41
SamuelChia <funwithes6@gmail.com>
2019-08-05 10:51:51 +0800

    Add 350. Intersection of Two Arrays II

--- algorithms/350/intersectionOfTwoArrays2.ts
+++ algorithms/350/intersectionOfTwoArrays2.ts
@@ -1,9 +1,27 @@
 type IntersectionOfTwoArraysFn = (nums1: number[], numss: number[]) => number[];
-/**
- * @param {number[]} nums1
- * @param {number[]} nums2
- * @return {number[]}
- */
-const intersectionOfTwoArraysFn: IntersectionOfTwoArraysFn = (nums1, nums2) => {
-  return [];
+
+export const intersectionOfTwoArraysFn: IntersectionOfTwoArraysFn = (nums1, nums2) => {
+  const results = [];
+  let lp = 0,
+    rp = 0;
+  //   sort
+  nums1.sort((a, b) => a - b);
+  nums2.sort((a, b) => a - b);
+  while (lp < nums1.length && rp < nums2.length) {
+    if (nums1[lp] < nums2[rp]) {
+      lp++;
+      continue;
+    }
+    if (nums1[lp] > nums2[rp]) {
+      rp++;
+      continue;
+    }
+    if (nums1[lp] === nums2[rp]) {
+      results.push(nums2[rp]);
+      lp++;
+      rp++;
+      continue;
+    }
+  }
+  return results;
 };
