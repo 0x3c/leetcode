131194cef6a516cb28d7f512545a86fce4bbac98
Samuel Graham <funwithes6@gmail.com>
2020-07-29 14:49:46 +0800

    Add 141. Linked List Cycle C++ solutions

--- algorithms/cpp/141.环形链表.cpp
+++ algorithms/cpp/141.环形链表.cpp
@@ -1,5 +1,5 @@
 /*
- * @lc app=leetcode.cn id=141 lang=javascript
+ * @lc app=leetcode.cn id=141 lang=cpp
  *
  * [141] 环形链表
  *
@@ -14,64 +14,68 @@
  * Testcase Example:  '[3,2,0,-4]\n1'
  *
  * 给定一个链表，判断链表中是否有环。
- *
+ * 
  * 为了表示给定链表中的环，我们使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。 如果 pos 是 -1，则在该链表中没有环。
- *
- *
- *
+ * 
+ * 
+ * 
  * 示例 1：
- *
+ * 
  * 输入：head = [3,2,0,-4], pos = 1
  * 输出：true
  * 解释：链表中有一个环，其尾部连接到第二个节点。
- *
- *
- *
- *
+ * 
+ * 
+ * 
+ * 
  * 示例 2：
- *
+ * 
  * 输入：head = [1,2], pos = 0
  * 输出：true
  * 解释：链表中有一个环，其尾部连接到第一个节点。
- *
- *
- *
- *
+ * 
+ * 
+ * 
+ * 
  * 示例 3：
- *
+ * 
  * 输入：head = [1], pos = -1
  * 输出：false
  * 解释：链表中没有环。
- *
- *
- *
- *
- *
- *
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
  * 进阶：
- *
+ * 
  * 你能用 O(1)（即，常量）内存解决此问题吗？
- *
- */
-/**
- * Definition for singly-linked list.
- * function ListNode(val) {
- *     this.val = val;
- *     this.next = null;
- * }
+ * 
  */
 
+// @lc code=start
 /**
- * @param {ListNode} head
- * @return {boolean}
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
  */
-var hasCycle = function(head) {
-  let walker = head,
-    runner = head;
-  while (runner && runner.next && runner.next.next) {
-    walker = walker.next;
-    runner = runner.next.next;
-    if (walker === runner) return true;
-  }
-  return false;
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        if (head == NULL)return false;
+        ListNode *walker = head;
+        ListNode *runner = head;
+        while (runner->next != NULL && runner->next->next != NULL) {
+            walker = walker->next;
+            runner = runner->next->next;
+            if (runner == walker)return true;
+        }
+        return false;
+    }
 };
+// @lc code=end
+
