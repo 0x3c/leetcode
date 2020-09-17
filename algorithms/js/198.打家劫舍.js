b1b8ac9930c1a415e2a5fee62b232c0467c59cbd
Samuel Chia <funwithes6@gmail.com>
2020-09-17 15:28:12 +0800

    Add javascript solution

---
+++ algorithms/js/198.打家劫舍.js
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode.cn id=198 lang=javascript
+ *
+ * [198] 打家劫舍
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var rob = function(nums) {
+  const len = nums.length;
+  const dp = Array.from({ length: len + 1 }, () => 0);
+  dp[1] = nums[0];
+  for (let i = 2; i <= len; i++) {
+    dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i - 1]);
+  }
+  return dp[len];
+};
+// @lc code=end
