0a738308feba1af0e7a775e0152957d794793b70
Samuel Chia <funwithes6@gmail.com>
2020-10-04 20:14:45 +0800

    Using bottom up

--- algorithms/js/62.不同路径.js
+++ algorithms/js/62.不同路径.js
@@ -3,59 +3,25 @@
  *
  * [62] 不同路径
  *
- * https://leetcode-cn.com/problems/unique-paths/description/
- *
- * algorithms
- * Medium (55.35%)
- * Likes:    314
- * Dislikes: 0
- * Total Accepted:    36.8K
- * Total Submissions: 65.7K
- * Testcase Example:  '3\n2'
- *
- * 一个机器人位于一个 m x n 网格的左上角 （起始点在下图中标记为“Start” ）。
- *
- * 机器人每次只能向下或者向右移动一步。机器人试图达到网格的右下角（在下图中标记为“Finish”）。
- *
- * 问总共有多少条不同的路径？
- *
- *
- *
- * 例如，上图是一个7 x 3 的网格。有多少可能的路径？
- *
- * 说明：m 和 n 的值均不超过 100。
- *
- * 示例 1:
- *
- * 输入: m = 3, n = 2
- * 输出: 3
- * 解释:
- * 从左上角开始，总共有 3 条路径可以到达右下角。
- * 1. 向右 -> 向右 -> 向下
- * 2. 向右 -> 向下 -> 向右
- * 3. 向下 -> 向右 -> 向右
- *
- *
- * 示例 2:
- *
- * 输入: m = 7, n = 3
- * 输出: 28
- *
  */
 
 /**
  * 第(i,j)个元素的路径总数等于第(i-1,j)和第(i,j-1)元素的路径总数之和。
- * 初始行所有元素路径总数为1。
+ * 终点元素路径总数为1。
  * @param {number} m
  * @param {number} n
  * @return {number}
  */
 var uniquePaths = function(m, n) {
-  const dp = Array.from({ length: n }, () => 1);
-  for (let i = 1; i < m; i++) {
-    for (let j = 1; j < n; j++) {
-      dp[j] += dp[j - 1];
+  const ans = Array.from({ length: n }, () =>
+    Array.from({ length: m }, () => 1)
+  );
+  const calc = (i, j) => (i >= n || j >= m ? 0 : ans[i][j]);
+  for (let i = n - 1; i >= 0; i--) {
+    for (let j = m - 1; j >= 0; j--) {
+      ans[i][j] =
+        i === n - 1 && j === m - 1 ? 1 : calc(i + 1, j) + calc(i, j + 1);
     }
   }
-  return dp[n - 1];
+  return ans[0][0];
 };
