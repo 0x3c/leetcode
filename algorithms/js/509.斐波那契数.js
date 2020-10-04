3cb570ec5faec8b9832d04e4f67a198d3fca41cb
Samuel Chia <funwithes6@gmail.com>
2020-10-04 19:41:25 +0800

    Modified using bottom up

--- algorithms/js/509.斐波那契数.js
+++ algorithms/js/509.斐波那契数.js
@@ -2,61 +2,10 @@
  * @lc app=leetcode.cn id=509 lang=javascript
  *
  * [509] 斐波那契数
- *
- * https://leetcode-cn.com/problems/fibonacci-number/description/
- *
- * algorithms
- * Easy (65.43%)
- * Likes:    60
- * Dislikes: 0
- * Total Accepted:    22.3K
- * Total Submissions: 34K
- * Testcase Example:  '2'
- *
- * 斐波那契数，通常用 F(n) 表示，形成的序列称为斐波那契数列。该数列由 0 和 1 开始，后面的每一项数字都是前面两项数字的和。也就是：
- *
- * F(0) = 0,   F(1) = 1
- * F(N) = F(N - 1) + F(N - 2), 其中 N > 1.
- *
- *
- * 给定 N，计算 F(N)。
- *
- *
- *
- * 示例 1：
- *
- * 输入：2
- * 输出：1
- * 解释：F(2) = F(1) + F(0) = 1 + 0 = 1.
- *
- *
- * 示例 2：
- *
- * 输入：3
- * 输出：2
- * 解释：F(3) = F(2) + F(1) = 1 + 1 = 2.
- *
- *
- * 示例 3：
- *
- * 输入：4
- * 输出：3
- * 解释：F(4) = F(3) + F(2) = 2 + 1 = 3.
- *
- *
- *
- *
- * 提示：
- *
- *
- * 0 ≤ N ≤ 30
- *
- *
  */
 
-// @lc code=start
 /**
- * 递归
+ * using memo
  * @param {number} N
  * @param {number[]} memo
  * @return {number}
@@ -64,6 +13,20 @@
 var fib = function(N, memo = [0, 1]) {
   if (N >= memo.length) {
     memo.push(fib(N - 1, memo) + fib(N - 2, memo));
+  }
+  return memo[N];
+};
+// @lc code=start
+/**
+ * 自底向上
+ * @param {number} N
+ * @return {number}
+ */
+var fib = function(N) {
+  const memo = [0, 1];
+  let i = 1;
+  while (++i < N + 1) {
+    memo[i] = memo[i - 1] + memo[i - 2];
   }
   return memo[N];
 };
