38e7a7404840b67bfef1610622b558efa551c1a0
Samuel Chia <funwithes6@gmail.com>
2020-10-05 15:07:34 +0800

    Modified

--- algorithms/js/62.不同路径.js
+++ algorithms/js/62.不同路径.js
@@ -13,15 +13,11 @@
  * @return {number}
  */
 var uniquePaths = function(m, n) {
-  const ans = Array.from({ length: n }, () =>
-    Array.from({ length: m }, () => 1)
-  );
-  const calc = (i, j) => (i >= n || j >= m ? 0 : ans[i][j]);
-  for (let i = n - 1; i >= 0; i--) {
+  const dp = Array.from({ length: m }, () => 1);
+  for (let i = n - 2; i >= 0; i--) {
     for (let j = m - 1; j >= 0; j--) {
-      ans[i][j] =
-        i === n - 1 && j === m - 1 ? 1 : calc(i + 1, j) + calc(i, j + 1);
+      dp[j] += dp[j + 1] || 0;
     }
   }
-  return ans[0][0];
+  return dp[0];
 };
