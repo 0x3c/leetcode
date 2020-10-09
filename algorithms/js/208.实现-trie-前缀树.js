0b0810c7864f53f4df7fe633b1fe45e4bc6d3198
Samuel Chia <funwithes6@gmail.com>
2020-10-09 14:51:40 +0800

    Modified code

--- algorithms/js/208.实现-trie-前缀树.js
+++ algorithms/js/208.实现-trie-前缀树.js
@@ -1,74 +1,28 @@
-/*
- * @lc app=leetcode.cn id=208 lang=javascript
- *
- * [208] 实现 Trie (前缀树)
- *
- * https://leetcode-cn.com/problems/implement-trie-prefix-tree/description/
- *
- * algorithms
- * Medium (60.52%)
- * Likes:    108
- * Dislikes: 0
- * Total Accepted:    11.4K
- * Total Submissions: 18.8K
- * Testcase Example:  '["Trie","insert","search","search","startsWith","insert","search"]\n' +
-  '[[],["apple"],["apple"],["app"],["app"],["app"],["app"]]'
- *
- * 实现一个 Trie (前缀树)，包含 insert, search, 和 startsWith 这三个操作。
- * 
- * 示例:
- * 
- * Trie trie = new Trie();
- * 
- * trie.insert("apple");
- * trie.search("apple");   // 返回 true
- * trie.search("app");     // 返回 false
- * trie.startsWith("app"); // 返回 true
- * trie.insert("app");   
- * trie.search("app");     // 返回 true
- * 
- * 说明:
- * 
- * 
- * 你可以假设所有的输入都是由小写字母 a-z 构成的。
- * 保证所有输入均为非空字符串。
- * 
- * 
- */
 /**
  * Initialize your data structure here.
  */
+
+// @lc code=start
+
 var Trie = function() {
-  this.trees = {};
+  this.isEnd = false;
+  this.trie = Array.from({ length: 26 }, (_) => null);
 };
 
-/**
- * 获取前缀树的枝叶
- * @param {string} prefix
- * @param {object} tree
- */
-Trie.prototype.getTrees = function(prefix, tree) {
-  if (!prefix) return tree;
-  if (!tree[prefix[0]]) return false;
-  return this.getTrees(prefix.slice(1), tree[prefix[0]]);
-};
-/**
- * 更新前缀树的枝叶
- * @param {string} prefix
- * @param {object} tree
- */
-Trie.prototype.setTrees = function(prefix, tree) {
-  if (!prefix) return (tree.isEnd = true);
-  if (!tree[prefix[0]]) tree[prefix[0]] = {};
-  return this.setTrees(prefix.slice(1), tree[prefix[0]]);
-};
 /**
  * Inserts a word into the trie.
  * @param {string} word
  * @return {void}
  */
 Trie.prototype.insert = function(word) {
-  this.setTrees(word, this.trees);
+  let head = this;
+  for (let ch of word) {
+    if (!head.trie[ch.charCodeAt() - "a".charCodeAt()]) {
+      head.trie[ch.charCodeAt() - "a".charCodeAt()] = new Trie();
+    }
+    head = head.trie[ch.charCodeAt() - "a".charCodeAt()];
+  }
+  head.isEnd = true;
 };
 
 /**
@@ -77,8 +31,14 @@ Trie.prototype.insert = function(word) {
  * @return {boolean}
  */
 Trie.prototype.search = function(word) {
-  const tree = this.getTrees(word, this.trees);
-  return tree ? !!tree.isEnd : false;
+  let head = this;
+  for (let ch of word) {
+    if (!head.trie[ch.charCodeAt() - "a".charCodeAt()]) {
+      return false;
+    }
+    head = head.trie[ch.charCodeAt() - "a".charCodeAt()];
+  }
+  return head.isEnd;
 };
 
 /**
@@ -87,10 +47,15 @@ Trie.prototype.search = function(word) {
  * @return {boolean}
  */
 Trie.prototype.startsWith = function(prefix) {
-  const tree = this.getTrees(prefix, this.trees);
-  return tree ? true : false;
+  let head = this;
+  for (let ch of prefix) {
+    if (!head.trie[ch.charCodeAt() - "a".charCodeAt()]) {
+      return false;
+    }
+    head = head.trie[ch.charCodeAt() - "a".charCodeAt()];
+  }
+  return true;
 };
-
 /**
  * Your Trie object will be instantiated and called as such:
  * var obj = new Trie()
@@ -98,3 +63,4 @@ Trie.prototype.startsWith = function(prefix) {
  * var param_2 = obj.search(word)
  * var param_3 = obj.startsWith(prefix)
  */
+// @lc code=end
