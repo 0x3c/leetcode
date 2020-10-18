a0351edba782bbf35fb90b4342b9f346be3db9b4
Samuel Chia <funwithes6@gmail.com>
2020-10-18 17:50:17 +0800

    Add 547. Friend Circles - using union find

--- algorithms/js/547.朋友圈.js
+++ algorithms/js/547.朋友圈.js
@@ -4,7 +4,10 @@
  * [547] 朋友圈
  */
 
-// @lc code=start
+/**
+ *  1. dfs
+ */
+
 /**
  * @param {number[][]} M
  * @param {number[]} visited
@@ -32,5 +35,54 @@ var findCircleNum = function(M) {
     }
   }
   return count;
+};
+
+// @lc code=start
+
+/**
+ * 2. 并查集
+ */
+class UnionFind {
+  constructor(n) {
+    this.count = n;
+    this.parent = Array.from({ length: n }, (_, i) => i);
+    this.size = Array.from({ length: n }, (_, i) => 1);
+  }
+  find(p) {
+    let parent = this.parent;
+    while (parent[p] !== p) {
+      parent[p] = parent[parent[p]];
+      p = parent[p];
+    }
+    return p;
+  }
+  union(p, q) {
+    const rootP = this.find(p);
+    const rootQ = this.find(q);
+    if (rootP === rootQ) return;
+    if (this.size[rootP] > this.size[rootQ]) {
+      this.parent[rootQ] = rootP;
+      this.size[rootP] += this.size[rootQ];
+    } else {
+      this.parent[rootP] = rootQ;
+      this.size[rootQ] += this.size[rootP];
+    }
+    this.count--;
+  }
+}
+
+/**
+ * @param {number[][]} M
+ * @return {number}
+ */
+var findCircleNum = function(M) {
+  const uf = new UnionFind(M.length);
+  for (let i = 0; i < M.length; i++) {
+    for (let j = 0; j < M.length; j++) {
+      if (M[i][j] === 0) continue;
+      uf.union(i, j);
+    }
+  }
+  return uf.count;
 };
 // @lc code=end
