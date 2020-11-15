bb1f0a3c50d8b11158368cbbbda99ca3ff2c6c62
Samuel Chia <funwithes6@gmail.com>
2020-11-15 16:17:13 +0800

    Add comments

--- algorithms/cpp/70.爬楼梯.cpp
+++ algorithms/cpp/70.爬楼梯.cpp
@@ -1,10 +1,32 @@
+#include <unordered_map>
+using namespace std;
 /*
  * @lc app=leetcode.cn id=70 lang=cpp
  *
  * [70] 爬楼梯
  */
 
+/**
+ * 递归并剪枝(缓存减少重复计算)
+ */
+class Solution2 {
+ private:
+  unordered_map<int, int> cache;
+
+ public:
+  int climbStairs(int n) {
+    if (n < 3) return n;
+    if (!cache.count(n)) {
+      cache[n] = climbStairs(n - 1) + climbStairs(n - 2);
+    }
+    return cache.at(n);
+  }
+};
+
 // @lc code=start
+/**
+ * 转化为 Fibonacci 问题
+ */
 class Solution {
  public:
   int climbStairs(int n) {
