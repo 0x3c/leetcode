79118de797d6cba659c05c10bad89d8e878a2b78
Samuel Chia <funwithes6@gmail.com>
2020-11-23 19:36:45 +0800

    Add 392. Is Subsequence

---
+++ algorithms/cpp/392.判断子序列.cpp
@@ -0,0 +1,25 @@
+#include <string>
+using namespace std;
+
+/*
+ * @lc app=leetcode.cn id=392 lang=cpp
+ *
+ * [392] 判断子序列
+ */
+
+// @lc code=start
+class Solution {
+ public:
+  bool isSubsequence(string s, string t) {
+    int lp = 0, rp = 0;
+    while (lp < s.size() && rp < t.size()) {
+      if (s[lp] == t[rp]) {
+        lp++;
+      }
+      rp++;
+    }
+    if (lp == s.size()) return true;
+    return false;
+  }
+};
+// @lc code=end
