c5886bcabca0a5affb674e129b0a958a34102e31
Samuel Chia <funwithes6@gmail.com>
2020-12-03 14:28:36 +0800

    Add 300. Longest Increasing Subsequence

---
+++ algorithms/js/300.最长上升子序列.js
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode.cn id=300 lang=javascript
+ *
+ * [300] 最长上升子序列
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var lengthOfLIS = function(nums) {
+  const len = nums.length;
+  const dp = Array.from({ length: len }, () => 1);
+  let ans = 1;
+  for (let i = 1; i < len; i++) {
+    for (let j = 0; j < i; j++) {
+      if (nums[i] > nums[j]) {
+        dp[i] = Math.max(dp[i], dp[j] + 1);
+      }
+    }
+    ans = Math.max(ans, dp[i]);
+  }
+  return ans;
+};
+// @lc code=end
