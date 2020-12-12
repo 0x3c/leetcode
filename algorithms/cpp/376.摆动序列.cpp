0f7b02820d42da1dc443d3d1fdb1545f3be40fcc
Samuel Chia <funwithes6@gmail.com>
2020-12-12 10:59:46 +0800

    Modified dp

--- algorithms/cpp/376.摆动序列.cpp
+++ algorithms/cpp/376.摆动序列.cpp
@@ -6,7 +6,6 @@ using std::vector;
  * [376] 摆动序列
  */
 
-// @lc code=start
 /**
  * Using DP
  * 维护一个上升摇摆序列 up[i] 和一个下降摇摆序列 down[i]
@@ -19,8 +18,11 @@ using std::vector;
  * down[i] 同上
  * 当 nums[i]<nums[i-1], down[i]=max(down[i-1], up[i-1]+1)
  * 当 nums[i]==nums[i-1], down[i]=down[i-1]
+ *
+ * 时间复杂度: O(n), 一次遍历即可,
+ * 空间复杂度: O(2n)
  */
-class Solution {
+class Solution_DP {
  public:
   int wiggleMaxLength(vector<int>& nums) {
     int len = nums.size();
@@ -39,5 +41,29 @@ class Solution {
     }
     return std::max(up[len - 1], down[len - 1]);
   }
+};
+
+// @lc code=start
+/**
+ * 对 dp 的空间优化
+ *
+ * 时间复杂度: O(n), 一次遍历即可,
+ * 空间复杂度: O(1)
+ */
+class Solution {
+ public:
+  int wiggleMaxLength(vector<int>& nums) {
+    int len = nums.size();
+    if (len < 2) return len;
+    int up = 1, down = 1;
+    for (int i = 1; i < len; i++) {
+      if (nums[i] > nums[i - 1]) {
+        up = std::max(up, down + 1);
+      } else if (nums[i] < nums[i - 1]) {
+        down = std::max(down, up + 1);
+      }
+    }
+    return std::max(up, down);
+  };
 };
 // @lc code=end
