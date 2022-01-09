7c3feeef282e3ebb83f74f14e0a47f26b292ca27
Samuel Chia <funwithes6@gmail.com>
2022-01-09 18:47:02 +0800

    Add 11. Container With Most Water with golang

---
+++ algorithms/golang/11.盛最多水的容器.go
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode.cn id=11 lang=golang
+ *
+ * [11] 盛最多水的容器
+ */
+
+// @lc code=start
+func min(x, y int) int {
+	if x < y {
+		return x
+	} else {
+		return y
+	}
+}
+
+func maxArea(height []int) int {
+	ans := 0
+	for left, right := 0, len(height)-1; left < right; {
+
+		temp := min(height[left], height[right]) * (right - left)
+		if temp > ans {
+			ans = temp
+		}
+		if height[left] > height[right] {
+			right--
+		} else {
+			left++
+		}
+	}
+	return ans
+}
+
+// @lc code=end
