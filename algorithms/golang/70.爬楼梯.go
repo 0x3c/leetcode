5964dd805415cf5192f540f4fe89ae73f2d10ce6
Samuel Chia <funwithes6@gmail.com>
2022-02-08 00:42:10 +0800

    Add 70. Climbing Stairs with recursive solution

--- algorithms/golang/70.爬楼梯.go
+++ algorithms/golang/70.爬楼梯.go
@@ -1,3 +1,5 @@
+package main
+
 /*
  * @lc app=leetcode.cn id=70 lang=golang
  *
@@ -5,6 +7,33 @@
  */
 
 // @lc code=start
+
+/**
+ * 递归 + 记忆
+ * 时间复杂度：O(n)
+ * 空间复杂度：O(n) - 递归栈
+ */
+func recursion(n int, cache map[int]int) int {
+	if cache[n] > 0 {
+		return cache[n]
+	}
+	ans := recursion(n-1, cache) + recursion(n-2, cache)
+	cache[n] = ans
+	return ans
+}
+func climbStairs_1(n int) int {
+	if n < 3 {
+		return n
+	}
+	cache := map[int]int{1: 1, 2: 2, 3: 3}
+	return recursion(n-1, cache) + recursion(n-2, cache)
+}
+
+/**
+ * 数学
+ * 时间复杂度：O(n)
+ * 空间复杂度：O(1)
+ */
 func climbStairs(n int) int {
 	if n < 3 {
 		return n
