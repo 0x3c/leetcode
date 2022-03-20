3ebfaeba5d392d56c40b8060793c792e1d07c5ed
Samuel Chia <funwithes6@gmail.com>
2022-03-20 18:42:44 +0800

    Modified func

--- algorithms/golang/102.二叉树的层序遍历.go
+++ algorithms/golang/102.二叉树的层序遍历.go
@@ -1,7 +1,7 @@
 /*
  * @lc app=leetcode.cn id=102 lang=golang
  *
- * [102] 二叉树的层次遍历
+ * [102] 二叉树的层序遍历
  */
 
 // @lc code=start
@@ -13,33 +13,34 @@
  *     Right *TreeNode
  * }
  */
+
+/**
+ * BFS
+ * 时间复杂度：O(n)
+ * 空间复杂度：O(n)
+ */
 func levelOrder(root *TreeNode) [][]int {
 	ans := [][]int{}
 	if root == nil {
 		return ans
 	}
-	queue := [][]*TreeNode{}
-	queue = append(queue, []*TreeNode{root})
+	queue := []*TreeNode{root}
+
 	for len(queue) > 0 {
-		task := queue[0]
-		queue = queue[1:]
-		cur_ans := []int{}
-		new_tasks := []*TreeNode{}
-		for i := 0; i < len(task); i++ {
-			cur_ans = append(cur_ans, task[i].Val)
-			if task[i].Left != nil {
-				new_tasks = append(new_tasks, task[i].Left)
+		size := len(queue)
+		ret := []int{}
+		for i := 0; i < size; i++ {
+			node := queue[i]
+			ret = append(ret, node.Val)
+			if node.Left != nil {
+				queue = append(queue, node.Left)
 			}
-			if task[i].Right != nil {
-				new_tasks = append(new_tasks, task[i].Right)
+			if node.Right != nil {
+				queue = append(queue, node.Right)
 			}
 		}
-		if len(new_tasks) > 0 {
-			queue = append(queue, new_tasks)
-		}
-		if len(cur_ans) > 0 {
-			ans = append(ans, cur_ans)
-		}
+		queue = queue[size:]
+		ans = append(ans, ret)
 	}
 	return ans
 }
