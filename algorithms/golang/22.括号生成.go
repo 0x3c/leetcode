333d86991afd6aa091ad82f6ae179bab7e1d93c0
Samuel Chia <funwithes6@gmail.com>
2022-03-20 23:21:13 +0800

    Add solution for BFS

--- algorithms/golang/22.括号生成.go
+++ algorithms/golang/22.括号生成.go
@@ -8,7 +8,7 @@
 /**
  * 递归 + 剪枝
  */
-func generateParenthesis(n int) []string {
+func generateParenthesis_1(n int) []string {
 	ans := []string{}
 	var dfs func(left, right int, cur string)
 	dfs = func(left, right int, cur string) {
@@ -24,6 +24,41 @@ func generateParenthesis(n int) []string {
 		}
 	}
 	dfs(n, n, "")
+	return ans
+}
+
+/**
+ * BFS
+ */
+func generateParenthesis(n int) []string {
+	ans := []string{}
+	q := []string{"("}
+	for len(q) > 0 {
+		size := len(q)
+		for i := 0; i < size; i++ {
+			node := q[i]
+			if len(node) == n*2 {
+				ans = append(ans, node)
+				continue
+			}
+			left, right := 0, 0
+			for _, ch := range node {
+				if ch == '(' {
+					left++
+				} else {
+					right++
+				}
+			}
+			if left > right {
+				q = append(q, string(append([]byte(node), ')')))
+			}
+			if left < n {
+				q = append(q, string(append([]byte(node), '(')))
+			}
+		}
+		q = q[size:]
+
+	}
 	return ans
 }
 
